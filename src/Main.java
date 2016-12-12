45b7306de5c5425623e33e1048d51fa204da89fc
Nhan Cao <nhancv1992@gmail.com>
2016-12-12 21:57:55 +0700

    update change file image file name as name of folder

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,70 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Files;
 
 public class Main {
 
     public static void main(String[] args) {
-        FileUtils fileUtils = new FileUtils("input.txt", "output.txt");
-        fileUtils.handle(new FileUtils.IFile() {
-            List<String> inputArray = new ArrayList<>();
+        File folder = new File("files");
+        File[] parentFolders = folder.listFiles();
 
-            @Override
-            public void iRead(final BufferedReader reader) {
-                reader.lines().forEach(s -> {
-                    inputArray.add(s);
-                });
-            }
+        if (parentFolders != null) {
+            for (File parentDir : parentFolders) {
+                if (parentDir.isDirectory()) {
+                    String newName = parentDir.getName();
+                    String fileExt = null;
+                    File[] lv1ListDir = parentDir.listFiles();
+                    if (lv1ListDir != null) {
+                        for (File file : lv1ListDir) {
+                            if (file.isDirectory()) {
 
-            @Override
-            public void iWrite(final BufferedWriter writer) {
-                inputArray.forEach((x) -> {
-                    try {
-                        writer.write("'" + x + "',\n");
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                                File[] lv2ListFile = file.listFiles();
+                                if (lv2ListFile != null) {
+                                    for (File file1 : lv2ListFile) {
+                                        if (file1.isFile()) {
+                                            String currentName = file1.getName();
+                                            if (fileExt == null) {
+                                                fileExt = getFileExtension(file1);
+                                                if (fileExt != null) {
+                                                    newName += "." + fileExt;
+                                                }
+                                            }
+                                            if (!currentName.startsWith(".DS_Store")) {
+                                                System.out.println(String.format("File %s to %s", currentName, newName));
+                                                renameFile(file1, newName);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                });
-
+                }
             }
-        });
+        }
 
     }
+
+    public static String getFileExtension(File file) {
+        try {
+            String fullFileName = file.getName();
+            String[] split = fullFileName.split("\\.");
+            if (split.length > 1) {
+                return split[split.length - 1];
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static void renameFile(File fileName, String newFileName) {
+        try {
+            File newFile = new File(fileName.getParent(), newFileName);
+            Files.move(fileName.toPath(), newFile.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
